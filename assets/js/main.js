d8501f4642b780e3bfc2f0a5dc60bffb5f0d1669
lekova <lina.lekova@gmail.com>
2015-09-19 11:23:24 -0400

    Add JS file

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) {
+	/*======= Skillset *=======*/
+	$('.level-bar-inner').css('width', '0');
+	$(window).on('load', function() {
+		$('.level-bar-inner').each(function() {
+			var itemWidth = $(this).data('level');
+			$(this).animate({
+				width: itemWidth
+			}, 800);
+		});
+	});
+
+	/* Bootstrap Tooltip for Skillset */
+	$('.level-label').tooltip();
+
+	GitHubActivity.feed({ username: 'lekova', selector: '#ghfeed', repository: 'game_set_match' });
+});
